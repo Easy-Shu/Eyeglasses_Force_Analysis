f825f20962da11dfeb60885469bf5d5eefb72389
EasyShu <60866625+Easy-Shu@users.noreply.github.com>
2023-06-09 14:50:33 +0800

    Add files via upload

--- Figure9_Temple_Comfort_Analysis.py
+++ Figure9_Temple_Comfort_Analysis.py
@@ -45,14 +45,14 @@ print(df_data.columns.values)
 
 #=========temple fit
 
-force_interval=0.025  #0.025 0.050 0.075  
+force_interval=0.075  #0.025 0.050 0.075  
 gap=0.01
 
 df_data["Int_Force"]=np.round(df_data["Clapping_Force"]/force_interval,0).astype(int)*force_interval
 
 
  
-df_templeFit=pd.melt(df_data[["ID","Int_Force",'Temple_Fit_S']],id_vars=["ID","Int_Force"])
+df_templeFit=pd.melt(df_data[["ID","Int_Force",'Temple_Comfort_S']],id_vars=["ID","Int_Force"])
 
 
 #df_templeFit=df_templeFit[(df_templeFit["Int_Force"].values>=0.1) & (df_templeFit["Int_Force"].values<0.6)]
@@ -81,9 +81,9 @@ df_templeFit_stat=df_templeFit.groupby(['Int_Force','variable'], as_index=False)
 model_S = np.poly1d(np.polyfit(df_templeFit_stat["Int_Force"],df_templeFit_stat["mean"], 1))   
 
 
-df_templeFit["variable"][df_templeFit["variable"]=="Temple_Fit_S"]="Static condition"
+df_templeFit["variable"][df_templeFit["variable"]=="Temple_Comfort_S"]="Static condition"
 #df_templeFit["variable"][df_templeFit["variable"]=="Temple_Fit_D"]="Dynamic condition"
-df_templeFit_stat["variable"][df_templeFit_stat["variable"]=="Temple_Fit_S"]="Static condition"
+df_templeFit_stat["variable"][df_templeFit_stat["variable"]=="Temple_Comfort_S"]="Static condition"
 #df_templeFit_stat["variable"][df_templeFit_stat["variable"]=="Temple_Fit_D"]="Dynamic condition"
 
 jitter= position_jitter(width = gap*0.5, height = 0.1)
@@ -94,20 +94,28 @@ jitter= position_jitter(width = gap*0.5, height = 0.1)
 
 df_templeFit_stat["x"]=np.arange(int(df_templeFit_stat.shape[0]))+1#np.repeat(range(int(df_templeFit_stat.shape[0]/2)),2)+1
 
-model_Global = ols(formula='mean ~ Int_Force', data=df_templeFit_stat).fit()#+C(x1):C(variable)
+#model_Global = ols(formula='mean ~ Int_Force', data=df_templeFit_stat).fit()#+C(x1):C(variable)
+model_Global = ols(formula='mean ~ Int_Force+I(Int_Force**3)', data=df_templeFit_stat).fit()#+C(x1):C(variable)
 print(model_Global.summary())
 
-polyline_x =np.r_[df_templeFit_stat["Int_Force"].min()-force_interval,
-                  df_templeFit_stat["Int_Force"],
-                  df_templeFit_stat["Int_Force"].max()+force_interval]# np.linspace(0.05, 0.6, 90)
+# polyline_x =np.r_[df_templeFit_stat["Int_Force"].min()-force_interval,
+#                   df_templeFit_stat["Int_Force"],
+#                   df_templeFit_stat["Int_Force"].max()+force_interval]# 
+
+polyline_x =np.linspace(df_templeFit_stat["Int_Force"].min()-force_interval, df_templeFit_stat["Int_Force"].max()+force_interval, 90)
 df_polylinet=pd.DataFrame(dict(Int_Force=polyline_x))
 df_lineG=pd.DataFrame(dict(x=polyline_x,y=model_Global.predict(df_polylinet)))
 
-df_lineG["x"]=np.r_[0,df_templeFit_stat["x"],df_templeFit_stat["x"].max()+1]
+print(df_lineG.loc[df_lineG.y.argmax(),])
+
+df_lineG["x"]=np.linspace(df_templeFit_stat["x"].min()-1, df_templeFit_stat["x"].max()+1, 90)
+#np.r_[0,df_templeFit_stat["x"],df_templeFit_stat["x"].max()+1]
+#df_lineG["y"][11]=-3
 
 x_labels=(df_templeFit_stat.Int_Force*100).astype(int)/100 
 x_labels2=np.repeat(None,x_labels.shape[0])
-x_labels2[[1,5,9,13,17]]=x_labels[[1,5,9,13,17]]
+
+#x_labels2[[1,5,9,13,17]]=x_labels[[1,5,9,13,17]]
 
 
 gg_templefit=(ggplot()
@@ -123,12 +131,11 @@ gg_templefit=(ggplot()
 +scale_fill_manual(values=["#02ACF4","#FA6263"],#s = 0.90, l = 0.65, h=0.0417,color_space='husl',
                 guide = guide_legend( direction = "vertical",nrow=2,title=""))
 +scale_y_continuous(breaks=range(-3, 4),limits =(-3,3.25))
-#+scale_x_continuous(breaks=np.arange(0.1, 0.7, force_interval))
-+scale_x_discrete(labels=x_labels2)#labels =x_labels2)
+#+scale_x_discrete(labels=x_labels2)#labels =x_labels2)
 #+guide_legend(nrow=2)
 #+facet_wrap("variable")
 +xlab("Temple clamping force (N)")
-+ylab("Perceived fit socre of the temple")
++ylab("Perceived comfort score")
 +theme_matplotlib()
 +theme(legend_position="none",#(0.35,0.22),
       #aspect_ratio =1.05,
